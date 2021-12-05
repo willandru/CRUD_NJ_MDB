246e21964e4acd97b66c09ade882127e3c7ec5ff
willandru <willandru@hotmail.com>
2021-12-05 15:27:04 -0500

    I am trying to fix the alert bars from /notes/add

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,36 @@
 const router = require('express').Router(); // Crear un objeto para facilitarme la creacion de rutas
 
 
+router.get('/notes/add', (req, res) =>{
+    res.render('notes/new-note')
+});
+
+router.post('/notes/new-note', (req, res) =>{
+    const{title, description}= req.body;
+
+    const errors = [];
+    if(!title){
+        errors.push({text: 'Please Write a Title'});
+    }
+    if(!description){
+        errors.push({text: 'Please Write a Description'});
+    }
+
+    if(errors.length>0){
+        res.render('notes/new-note',{
+            errors,
+            title,
+            description
+        })
+    }else{
+        res.send('Ok');
+    }
+
+    
+});
+
+
+
 router.get('/notes', (req, res) => {
     res.send('Notas desde la BASE DE DATOS ... ');
 });
